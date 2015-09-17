98dd34a89bc1e39ec46a9595cc4bf679f900cdd3
alumno <alumno@HUEB03.UMG.ACADEMICO>
2015-09-16 20:01:52 -0600

    Programa de Cristopher HolaMundo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -13,6 +16,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "Hola vacas!!!...");
         // TODO code application logic here
     }
 }
