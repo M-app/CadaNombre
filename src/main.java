c8c2531e073970dc0f7bfbf4b3b0a21874ab52b8
alumno <alumno@HUEC04.UMG.ACADEMICO>
2015-09-16 20:07:57 -0600

    Tabla del 5

---
+++ src/main.java
@@ -0,0 +1,21 @@
+
+
+public class main 
+
+{
+
+   
+    public static void main(String[] args) {
+        
+        
+        int ciclo,resultado,numero;
+            numero = 5;
+            for(ciclo=1;ciclo<=10;ciclo=ciclo+1)
+                {
+                       resultado = numero * ciclo;
+                       System.out.println(numero + "*"+ ciclo + "=" + resultado);
+                }
+       
+    }
+
+}
