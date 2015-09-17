382ce7a140881b9284acc6446837b41add70715e
alumno <alumno@HUEB03.UMG.ACADEMICO>
2015-09-16 20:28:37 -0600

    Programa de Cristopher HolaMundo

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        JOptionPane.showMessageDialog(null, "Hola vacas!!!...");
+        JOptionPane.showMessageDialog(null, "Hola vacas!!!....");
         // TODO code application logic here
     }
 }
