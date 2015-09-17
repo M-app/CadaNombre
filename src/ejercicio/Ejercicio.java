b8cb3c60d97bde7a501b0af6c27f63d3a6c62063
alumno <alumno@HUEA01.UMG.ACADEMICO>
2015-09-16 20:31:25 -0600

    hace cosas sabrosas

---
+++ src/ejercicio/Ejercicio.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        inico frmPrincipal = new inico();
+      
+      frmPrincipal.setVisible(true);
+    }
+}
