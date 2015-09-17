0b34704e800fa1b6335f4a198312f016f998358e
alumno <alumno@HUEC05.UMG.ACADEMICO>
2015-09-16 20:17:48 -0600

    ojala de jaja

---
+++ src/fechaactual/FechaActual.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fechaactual;
+
+/**
+ *
+ * @author alumno
+ */
+public class FechaActual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        JFPrincipal frmprincipal = new JFPrincipal();
+        frmprincipal.setVisible(true);
+        frmprincipal.setLocationRelativeTo(null);
+        
+    }
+}
